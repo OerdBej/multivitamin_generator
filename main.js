2dd145bd38197a7c26b25eb3404500938aa1f157
Moran <thaler.moran@gmail.com>
2022-03-14 17:13:27 +0100

    added some html and js codes

---
+++ main.js
@@ -0,0 +1,31 @@
+// console.log("hello");
+
+// function hello() {
+//   console.log("Hi there!");
+// }
+
+//! Storing the data
+
+const VitaminC = [
+  { name: "Orange", quantity: 10 },
+  { name: "Kiwi", quantity: 0 },
+];
+
+const VitaminA = [
+  { name: "Carrot", quantity: 0 },
+  { name: "Sweet potato", quantity: 0 },
+];
+
+//* “ Your recipe for today is …..”
+//* how to get a value from a property
+//console.log(VitaminC[0].name); //calling ingredients
+
+//*get orange quantity according to the  position
+//* returning the quantity!!!
+//console.log(VitaminC[0].quantity);
+
+//* according to a prompt to return the quantity of 1 element
+
+// let which;
+const fruit1 = prompt("Which is your favorite fruit");
+console.log(fruit1);

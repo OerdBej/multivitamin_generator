35f0cf6c549ee6807247d842792011d77527fbb2
Moran <thaler.moran@gmail.com>
2022-03-21 23:54:38 +0100

    strat to create the css file, added also an image folder with all the images and icones for the project

--- main.js
+++ main.js
@@ -55,3 +55,22 @@ ingridientList.forEach((ingridient) => {
 
 //TODO Getting the elements in a new Recepie Array by check v
 //TODO To add Btn and EventListener // Start - continue - continue - Mix it -- Delay Screen shows loading page - Show the recepie
+
+// ANIMATED BTN
+
+var animateButton = function (e) {
+  e.preventDefault;
+  //reset animation
+  e.target.classList.remove("animate");
+
+  e.target.classList.add("animate");
+  setTimeout(function () {
+    e.target.classList.remove("animate");
+  }, 700);
+};
+
+var bubblyButtons = document.getElementsByClassName("bubbly-button");
+
+for (var i = 0; i < bubblyButtons.length; i++) {
+  bubblyButtons[i].addEventListener("click", animateButton, false);
+}
